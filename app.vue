66a6bab41fb228ffe44de1569dd3ea5a39ba5151
SamiFraca <fracasami@gmail.com>
2024-11-07 14:59:52 +0100

    Add router

--- app.vue
+++ app.vue
@@ -1,14 +1,7 @@
-<script setup lang="ts">
-import { useTestStore } from "@/store/testStore";
-const testStore = useTestStore();
-console.log(testStore)
-</script>
-
 <template>
   <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome /> -->
-    <h1>{{ testStore.upper }}</h1>
-    <button @click="testStore.setMessage('new message')">Set new message</button>
+    <NuxtLayoutHeader />
+    <NuxtPage />
+    <NuxtLayoutFooter />
   </div>
 </template>
---
+++ pages/form.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1>test</h1>
+  </div>
+</template>
