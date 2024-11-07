d4e62f8642ff2cb532d364cea067759e6c09b449
SamiFraca <fracasami@gmail.com>
2024-11-07 15:11:20 +0100

    Add simple email validation

--- components/form-component.vue
+++ components/form-component.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
-const onSubmit = (values:any) => {
+import { toTypedSchema } from "@vee-validate/zod";
+import * as zod from "zod";
+const fieldSchema = toTypedSchema(
+  zod.string().min(1, "Field is required").email("Must be a valid email")
+);
+const onSubmit = (values: any) => {
   console.log(values);
 };
 </script>
 
 <template>
   <div>
-    <VeeForm  @submit="onSubmit">
-      <VeeField type="email" name="email" />
-      <button>Sign up </button>
+    <VeeForm @submit="onSubmit">
+      <VeeField type="email" name="email" :rules="fieldSchema" />
+      <VeeErrorMessage name="email" />
+      <button>Sign up</button>
     </VeeForm>
   </div>
 </template>
