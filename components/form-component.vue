bb17125f0da478d30313656e9683fec9f77f957c
SamiFraca <fracasami@gmail.com>
2024-11-07 15:15:52 +0100

    Add multiple zod values

--- components/form-component.vue
+++ components/form-component.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import { toTypedSchema } from "@vee-validate/zod";
 import * as zod from "zod";
-const fieldSchema = toTypedSchema(
-  zod.string().min(1, "Field is required").email("Must be a valid email")
+const validationSchema = toTypedSchema(
+  zod.object({
+    email: zod
+      .string()
+      .min(1, { message: "This is required" })
+      .email({ message: "Must be a valid email" }),
+    password: zod
+      .string()
+      .min(1, { message: "This is required" })
+      .min(8, { message: "Too short" }),
+  })
 );
 const onSubmit = (values: any) => {
   console.log(values);
@@ -11,10 +20,26 @@ const onSubmit = (values: any) => {
 
 <template>
   <div>
-    <VeeForm @submit="onSubmit">
-      <VeeField type="email" name="email" :rules="fieldSchema" />
+    <VeeForm
+      :validation-schema="validationSchema"
+      @submit="onSubmit"
+      class="vee-form"
+    >
+      <label for="email">Email</label>
+      <VeeField type="email" name="email" />
       <VeeErrorMessage name="email" />
+      <label for="password">Password</label>
+      <VeeField type="password" name="password" />
+      <VeeErrorMessage name="password" />
       <button>Sign up</button>
     </VeeForm>
   </div>
 </template>
+<style>
+.vee-form {
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+  max-width: max-content;
+}
+</style>
