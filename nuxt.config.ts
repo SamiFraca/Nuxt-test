9559888e56793ec0b81a92bfe1f76d9b668ab235
SamiFraca <fracasami@gmail.com>
2024-11-08 09:39:00 +0100

    Add tailwind and shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,18 @@ export default defineNuxtConfig({
         },
       },
     ],
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
   ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
 });
